86a02e2bc1ca52221e9194b8cf8e05267e88829b
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2020-08-22 15:52:53 +0530

    90% done

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import pygame
 from colors import colors
 from node import make_grid
 
+# making the grid
 grid_len = 20
-
 main_grid = make_grid(grid_len)
 
+# pygame config
 pygame.init()
 WIN_WIDTH = 600
 WIN_HEIGHT = 600
@@ -30,6 +31,100 @@ def display_grid(grid):
 
 display_grid(main_grid)
 
+def show_steps(grid):
+    cell_width = WIN_WIDTH // grid_len
+    cell_height = WIN_HEIGHT // grid_len
+
+    for row in range(grid_len):
+        for col in range(grid_len):
+
+            if grid[row][col].isOpen is None:
+                color = colors['white']
+
+            else:
+                color = colors['green'] if grid[row][col].isOpen else colors['red']
+
+            pygame.draw.rect(
+                window, 
+                color, 
+                (
+                    row * cell_height + 2.5, 
+                    col * cell_width + 2.5,   
+                    cell_width - 5, 
+                    cell_height - 5
+                )
+            )
+    
+    pygame.display.update()
+
+# def see_neighbors():
+#     for i in range(len(main_grid)):
+#             for j in range(len(main_grid[i])):
+#                 for neighbor in main_grid[i][j].neighbors:
+#                     print(f'neighbor for {i}{j} = {neighbor.i}, {neighbor.j}', end = ' ')
+#                print("")
+
+def getHScore(node, endNode):
+    return abs(node.i - endNode.i) + abs(node.j - endNode.j)
+
+
+# A* PATHFINDING ALGORITHM
+
+def aStar(start_node, end_node):
+    # node.f = node.g + node.h
+    # node.g = distance of current node from the starting node
+    # node.h = distance of current node from the end node
+
+    # closed_set = []
+    start_node.g = 0
+    start_node.f = start_node.g + getHScore(start_node, end_node)
+    open_set = [start_node]
+
+
+    while len(open_set) > 0:
+        # print(open_set)
+        # for i in range(len(main_grid)):
+        #     for j in range(len(main_grid[i])):
+
+        current_node = open_set[0]
+
+        for node in open_set:
+            if node.f < current_node.f:
+                current_node = node
+                current_node.isOpen = True
+
+        print(f'current_node = {current_node.i, current_node.j}')
+
+        if current_node == end_node:
+            return
+
+        open_set.remove(current_node)
+        current_node.isOpen = False
+
+        for neighbor in current_node.neighbors:
+            # assuming 1 as the distance btw tow neighbouing poitns
+            print(f'neighbor = {neighbor}')
+
+            tempG = current_node.g + 1
+
+            if tempG < neighbor.g:
+                neighbor.g = tempG
+                neighbor.h = getHScore(neighbor, end_node)
+                neighbor.f = neighbor.g + neighbor.h
+
+            if neighbor not in open_set:
+                open_set.append(neighbor)
+                neighbor.isOpen = True
+
+        print(f'openSet = {open_set}')
+
+        show_steps(main_grid)
+
+
+
+aStar(main_grid[0][0], main_grid[19][19])
+# see_neighbors()
+
 # MAIN LOOP
 
 run = True
@@ -38,3 +133,6 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    
+    
--- node.py
+++ node.py
@@ -1,11 +1,32 @@
 class Node():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
         self.isWall = False
+        self.isOpen = None
         self.f = 0
         self.g = 0
         self.h = 0
+        self.neighbors = []
+
+    
+    def add_neighbors(self, grid):
+        i = self.i
+        j = self.j
+
+        if i > 0:
+            self.neighbors.append(grid[i - 1][j])
+
+        if i < len(grid) - 1:
+            self.neighbors.append(grid[i + 1][j])
+
+
+        if j > 0:
+            self.neighbors.append(grid[i][j - 1])
+
+        if j < len(grid) - 1:
+            self.neighbors.append(grid[i][j + 1])
+
 
 
 def make_grid(length):
@@ -13,8 +34,16 @@ def make_grid(length):
     for i in range(length):
         lst = []
         for j in range(length):
-            lst.append(Node(i, j))
+            node = Node(i, j)
+
+            lst.append(node)
 
         main_grid.append(lst)
 
+
+    for i in range(length):
+        for j in range(length):
+            main_grid[i][j].add_neighbors(main_grid)
+
     return main_grid
+
